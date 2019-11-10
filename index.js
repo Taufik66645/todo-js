33c2c8b7fc08ff326028b58a0828e2842c194ef0
taufik66645 <taufik.66645@gmail.com>
2019-11-10 12:57:27 +0700

    create new file

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+
+function addList (){
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let list = document.createTextNode(inputValue);
+    li.appendChild(list);
+    
+
+        if(inputValue === ''){
+            alert("Silahkan diisi");
+        }
+        else {
+            document.getElementById("ulList").appendChild(li);
+        }
+
+        document.getElementById("myInput").value ="";
+    }
+
+    const close = document.getElementsByClassName("close");
+DeviceLightEvent = i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
